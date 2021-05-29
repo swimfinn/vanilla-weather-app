6598398546ccbdd2c0171f7f5882e3a0b57153fc
swimfinn <swimfinn81@gmail.com>
2021-05-29 14:50:31 -0700

    styled the main elements of the app

---
+++ src/app.js
@@ -0,0 +1,80 @@
+function formatDate(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let dayIndex = date.getDay();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[dayIndex];
+
+    return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+    document.querySelector("#city-input").innerHTML = response.data.name;
+    document.querySelector("#currentTemp").innerHTML = Math.round(
+        response.data.main.temp
+    );
+    document.querySelector("#humidity-input").innerHTML =
+        response.data.main.humidity;
+    document.querySelector("#wind-input").innerHTML = Math.round(
+        response.data.wind.speed
+    );
+    document.querySelector("#weather-description").innerHTML =
+        response.data.weather[0].main;
+}
+
+function searchCity(city) {
+    let apiKey = "3a8d7f059fc61ac00591426445cb607a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+    debugger;
+    event.preventDefault();
+    let city = document.querySelector("#search-text-input").value;
+    searchCity(city);
+}
+
+function searchLocation(position) {
+    let apiKey = "3a8d7f059fc61ac00591426445cb607a";
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+    let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let dateElement = document.querySelector("#timeOfDay");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-bar");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", handleSubmit);
+
+searchCity("Portland");
