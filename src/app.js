bf0f1eaeade0991debc2aee70e3b3ed3ccda7821
swimfinn <swimfinn81@gmail.com>
2021-05-30 08:39:07 -0700

    added console log back to change icons

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ function formatDate(date) {
 }
 
 function displayWeatherCondition(response) {
+    console.log(response.data);
     document.querySelector("#city-input").innerHTML = response.data.name;
     document.querySelector("#currentTemp").innerHTML = Math.round(
         response.data.main.temp
